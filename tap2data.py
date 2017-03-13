cfdac71416aec284731106aa4183ecf14fbea720
pyrex8 <ev64bug@yahoo.com>
2017-03-12 20:21:27 -0700

    Cleaned up with pylint

--- tap2data.py
+++ tap2data.py
@@ -1,24 +1,28 @@
+# -*- coding: utf-8 -*-
+"""TAP file to data print utility. This is just to see the data in a .TAP file
 
-# Bytes: 
-# $0000-$000B: File signature "C64-TAPE-RAW"
-#        $000C: TAP version (see below for description)
-#               $00 - Original layout
-#               $01 - Updated
-# $000D-$000F: Future expansion
-# $0010-$0013: File  data  size  (not  including  this  header,  in
-#              LOW/HIGH format) i.e. This image is $00082151  bytes
-#                        long.
-# $0014-$xxxx: File data
+Bytes:
+$0000-$000B: File signature "C64-TAPE-RAW"
+        $000C: TAP version (see below for description)
+               $00 - Original layout
+               $01 - Updated
+ $000D-$000F: Future expansion
+ $0010-$0013: File  data  size  (not  including  this  header,  in
+              LOW/HIGH format) i.e. This image is $00082151  bytes
+                        long.
+ $0014-$xxxx: File data"""
 
+def main():
+    """ Main"""
+    with open("print_h.tap", "rb") as tap_file:
+        file_signature = tap_file.read(12)
+        print file_signature
+        tap_version = tap_file.read(3)
+        print 'TAP Version', ord(tap_version[0])
+#        future_expansion = tap_file.read(2)
+        file_size = tap_file.read(4)
+        print "size: $%02x%02x%02x%02x" % \
+                 (ord(file_size[3]), ord(file_size[2]), ord(file_size[1]), ord(file_size[0]))
 
-
-with open("print_h.tap", "rb") as f:
-    fileSignature = f.read(12)
-    print fileSignature
-    tapVersion = f.read(1)
-    print 'TAP Version',ord(tapVersion)
-    futureExpansion = f.read(2)
-    fileSize = f.read(4)
-    #    totalSize = ord(fileSize[3])+ord(fileSize[2])+ord(fileSize[1]),hex(ord(fileSize[0]))
-    print "size: $%02x%02x%02x%02x" % (ord(fileSize[3]),
-    	ord(fileSize[2]), ord(fileSize[1]), ord(fileSize[0]))
+if __name__ == '__main__':
+    main()
